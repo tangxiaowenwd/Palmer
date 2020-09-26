a09a97c048cfed9ac2129ec3c6e0020e3f9a518d
TWX <1285337818@qq.com>
2020-09-26 23:54:39 +0800

    修改

--- Finance/K线图.py
+++ Finance/K线图.py
@@ -8,23 +8,22 @@ import pandas as pd
 import matplotlib.pyplot as mp
 import datetime as dt
 import matplotlib.dates as md
+import re
 
 
 #日期转换函数
 def dmy2ymd(dmy):
     dmy = str(dmy,encoding='utf-8')
 
-
-class Main():
-    def __init__(self):
-        self.pro = ts.pro_api("d9f49767544519208fbf91e00a109558fe92e84bdcb70c9173144c24")
-
-    def main(self):
-        df = self.pro.query('daily', ts_code='000005.SZ')[:40]
-        print(df)
-        df["trade_date"] = pd.to_datetime(df["trade_date"],errors="ignore")
-        mp.figure("000001.SZ",facecolor="lightgray")
-        mp.title("000001.SZ",fontsize=16)
+class Graphic():
+    def __init__(self,df,name):
+        self.df = df
+        self.name = name
+        self.dates = np.array(pd.to_datetime(self.df["trade_date"],errors="ignore"))
+        mp.rcParams['font.family'] = ['sans-serif']
+        mp.rcParams['font.sans-serif'] = ['SimHei']
+        mp.figure(name,facecolor="lightgray")
+        mp.title(self.name)
         mp.xlabel("Date",fontsize=14)
         mp.ylabel("closing price",fontsize=14)
         mp.grid(linestyle=":")
@@ -33,28 +32,87 @@ class Main():
         #设置主刻度定位器为周定位器（每周显示主刻度文本）
         ax.xaxis.set_major_locator(md.WeekdayLocator(byweekday=md.MO))
         ax.xaxis.set_major_formatter(md.DateFormatter('%d-%m-%Y'))
-        #设置次定位器为日定位器
-        #ax.xaxis.set_major_locator(md.DayLocator(md.DayLocator()))
+        mp.plot(self.dates, df["close"], color="black", label=self.name, linestyle="-", linewidth=2, alpha=1)
 
-        #整理颜色
-        rise = df['close'] > df['open']
-        color = ["white" if x else 'green' for x in rise]
-        ecolor = ["red" if x else 'green' for x in rise]
-        mp.plot(df["trade_date"],df["close"],color="red",label="000001.sz",linestyle="--",linewidth=2,alpha=0.3)
-
-        #绘制K线图
-        mp.bar(df["trade_date"],df['change'],width=0.8,bottom=df["open"],color=color,edgecolor=ecolor,zorder=3)
-        #绘制影线
-        mp.vlines(df['trade_date'],df['low'],df['high'],color=ecolor)
+        #self.K_line()
+        self.Mean_line()
+        self.Monvolve()
+        self.Mean_move_line()
 
         mp.plot()
         mp.legend()
         mp.gcf().autofmt_xdate()
         mp.show()
 
+    def K_line(self):
+        # #绘制K线图
+        rise = self.df['close'] > self.df['open']
+        color = ["white" if x else 'green' for x in rise]
+        ecolor = ["red" if x else 'green' for x in rise]
+        mp.bar(self.dates,self.df['change'],width=0.8,bottom=self.df["open"],color=color,edgecolor=ecolor,zorder=3)
+        #绘制影线
+        mp.vlines(self.dates,self.df['low'],self.df['high'],color=ecolor)
+
+    def Mean_line(self,):
+        #算术平均值：当前一组数据无偏估计
+        mean = np.mean(self.df["close"])
+        print("算术平均值为：",mean)
+        mp.hlines(mean,self.dates[0],self.dates[-1],color='blue',label="mean",linestyles=":",alpha=0.5)
+    def Mean_move_line(self,days=10,weight=False):
+        #移动均线
+        ma5 = np.zeros(self.df['close'].size - days)
+        for i in range(ma5.size):
+            data = self.df['close'][i:i+days]
+            ma5[i] = data.mean()
+        mp.plot(self.dates[days:],ma5,color='blue',label="Mean-5",linestyle="--",linewidth=2,alpha=0.8)
+
+    def Monvolve(self,days=5,weight=False):
+        if weight:
+            # 加权卷积运算
+            x = np.linspace(0,1,days)
+            kernel1 =np.exp(x)
+            kernel1=kernel1/kernel1.sum()
+            print(kernel1)
+            sma53 = np.convolve(self.df["close"], kernel1, 'valid')
+            mp.plot(self.dates[days-1:], sma53, color='orange', label="加权卷积_5", linestyle="--", linewidth=2, alpha=0.9)
+        else:
+            # 卷积运算
+            kernel = np.ones(days) / days
+            sma52 = np.convolve(self.df["close"], kernel, 'valid')
+            mp.plot(self.dates[days - 1:], sma52, color='green', label="卷积_5", linestyle="--", linewidth=2, alpha=0.9)
+
+
+class GetInfo():
+    def __init__(self,ts_code):
+        self.pro = ts.pro_api("d9f49767544519208fbf91e00a109558fe92e84bdcb70c9173144c24")
+        self.ts_code = ts_code
+
+    def get_info(self):
+        data = self.pro.query('stock_basic', exchange='', list_status='L',
+                         fields='ts_code,symbol,name,area,industry,list_date')
+        #data.to_excel("ts_code.xls")
+        code_info = data[data["ts_code"]==self.ts_code]
+        # if not code_info.bool():
+        #     raise Exception("没有找到%s得相关信息" %self.ts_code)
+        name = self.convert_str(code_info["name"])
+        return name
+
+    def convert_str(self,strs):
+        regex = re.compile("\s+")
+        name = regex.split(str(strs).split("\n")[0])[1]
+        return name
+
+
+    def main(self):
+        df = self.pro.query('daily', ts_code=self.ts_code)[:60]
+        name = self.get_info()
+        return name,df
+
+
 
 
 
 if __name__ == "__main__":
-    main = Main()
-    main.main()
+    main = GetInfo(ts_code="600519.SH")
+    name,df = main.main()
+    Graphic(df,name)
