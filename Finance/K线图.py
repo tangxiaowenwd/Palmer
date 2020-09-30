9d32badb02fbd5707e251fceb3b10bb964ca7075
TWX <1285337818@qq.com>
2020-09-30 22:19:53 +0800

    修改

--- Finance/K线图.py
+++ Finance/K线图.py
@@ -32,17 +32,41 @@ class Graphic():
         #设置主刻度定位器为周定位器（每周显示主刻度文本）
         ax.xaxis.set_major_locator(md.WeekdayLocator(byweekday=md.MO))
         ax.xaxis.set_major_formatter(md.DateFormatter('%d-%m-%Y'))
-        mp.plot(self.dates, df["close"], color="black", label=self.name, linestyle="-", linewidth=2, alpha=1)
+        mp.plot(self.dates, df["close"], color="black", label=self.name, linestyle="-", linewidth=1, alpha=0.3)
 
         #self.K_line()
-        self.Mean_line()
-        self.Monvolve()
-        self.Mean_move_line()
+        # self.Mean_line()
+        # self.Monvolve()
+        # self.Mean_move_line()
+        # self.Bulindai()
+
+        self.line_p()
 
         mp.plot()
         mp.legend()
         mp.gcf().autofmt_xdate()
         mp.show()
+
+    def line_p(self):
+    #     A = np.zeros((3,3))
+    #     for j in range(3):
+    #         A[j,] = self.df["close"][j:j+3]
+    #     B = self.df["close"][3:6]
+    #     x = np.linalg.lstsq(A,B)[0]
+    #     print(x)
+    #     print(B.dot(x))
+    #     print(self.df["close"][6])
+        trends = (self.df["high"]+self.df["low"] + self.df["close"]) / 3
+        mp.scatter(self.dates,trends,marker="o",color="red",s=30,label="trends")
+        A = np.ones(self.df["close"].size)
+        days = self.dates.astype('M8[D]').astype('int32')
+        A = np.column_stack((days,np.ones_like(days)))
+        x = np.linalg.lstsq(A,self.df["close"])[0]
+        tr = x[0]*days+x[1]
+        mp.plot(self.dates,tr,color='green')
+
+
+
 
     def K_line(self):
         # #绘制K线图
@@ -74,12 +98,26 @@ class Graphic():
             kernel1=kernel1/kernel1.sum()
             print(kernel1)
             sma53 = np.convolve(self.df["close"], kernel1, 'valid')
-            mp.plot(self.dates[days-1:], sma53, color='orange', label="加权卷积_5", linestyle="--", linewidth=2, alpha=0.9)
+            mp.plot(self.dates[:-(days-1)], sma53, color='orange', label="加权卷积_5", linestyle="--", linewidth=2, alpha=0.9)
         else:
             # 卷积运算
             kernel = np.ones(days) / days
             sma52 = np.convolve(self.df["close"], kernel, 'valid')
-            mp.plot(self.dates[days - 1:], sma52, color='green', label="卷积_5", linestyle="--", linewidth=2, alpha=0.9)
+            mp.plot(self.dates[:-(days-1)], sma52, color='green', label="卷积_5", linestyle="--", linewidth=2, alpha=0.9)
+
+    def Bulindai(self,days=10):
+        kernel = np.ones(days) / days
+        sma52 = np.convolve(self.df["close"], kernel, 'valid')
+        mp.plot(self.dates[:-(days-1)], sma52, color='green', label="卷积_5", linestyle="--", linewidth=1, alpha=0.5)
+        stds = np.zeros(sma52.size)
+        for i in range(stds.size):
+            stds[i] = self.df["close"][i:i+days].std()
+        upper = sma52 + 2 * stds
+        lower = sma52 - 2 * stds
+        mp.plot(self.dates[:-(days-1)],upper,color='red',label="Upper")
+        mp.plot(self.dates[:-(days-1)], lower, color='red', label="Lower")
+        mp.fill_between(self.dates[:-(days-1)],upper,lower,lower<upper,color='orangered',alpha=0.2)
+
 
 
 class GetInfo():
@@ -107,6 +145,11 @@ class GetInfo():
         df = self.pro.query('daily', ts_code=self.ts_code)[:60]
         name = self.get_info()
         return name,df
+
+
+class StatistacalIndex():
+    def __init__(self):
+        pass
 
 
 
